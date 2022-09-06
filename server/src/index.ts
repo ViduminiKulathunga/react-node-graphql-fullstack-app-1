0955bb28912b53616ecfae6a966ce81513f77131
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-09-06 07:39:47 +0200

    rest api routes finished

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,35 +1,31 @@
 import express from 'express';
-import bodyParser from "body-parser";
-import {listings} from "./listings";
+import bodyParser from 'body-parser';
+import { listings } from './listings';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-const one: number = 8;
-const two: number = 11;
-const three: number = 100;
-
 app.get('/', (_req, res) => res.send(`one + two = ${one + two}`));
 
 // Listings
-app.get("/listings", (_req, res) => {
+app.get('/listings', (_req, res) => {
   return res.send(listings);
-})
+});
 
 //Delete Listing
-app.post("/delete-listings", (req, res) => {
+app.post('/delete-listings', (req, res) => {
   const id: string = req.body.id;
 
-  for(let i = 0; i < listings.length; i++ ){
-    if(listings[i].id === id){
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
       return res.send(listings.splice(i, 1));
     }
   }
 
-  return res.send("Failed to delete thing");
-})
+  return res.send('Failed to delete thing');
+});
 
 app.listen(port);
 
