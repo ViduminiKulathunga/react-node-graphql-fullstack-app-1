3213315f30a43fbb0d984803fe1d53103ee3248d
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-09-11 21:01:25 +0200

    using the graphql schema language

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { schema } from './graphql';
+import { typeDefs, resolvers } from './graphql';
 
 const app = express();
 const port = 3000;
 
 async function start() {
-  const server = new ApolloServer({ schema });
+  const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
   server.applyMiddleware({ app, path: '/api' });
 }
