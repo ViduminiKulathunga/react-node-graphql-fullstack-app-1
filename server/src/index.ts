41f044dd14ffd65f60b3edd5b39f3ec0897b2d0b
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-09-11 14:54:18 +0200

    create graphql schema with graphql library

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,16 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-
-app.get('/', (_req, res) => res.send(`one + two = ${one + two}`));
-
-// Listings
-app.get('/listings', (_req, res) => {
-  return res.send(listings);
-});
-
-//Delete Listing
-app.post('/delete-listings', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send('Failed to delete thing');
-});
+async function start() {
+  const server = new ApolloServer({ schema });
+  await server.start();
+  server.applyMiddleware({ app, path: '/api' });
+}
+start();
 
 app.listen(port);
 
