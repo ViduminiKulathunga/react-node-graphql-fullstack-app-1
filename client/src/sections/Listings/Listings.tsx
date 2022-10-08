2e9aa3c4a5d25742ebc240e640c2ced2eef5c1ac
ViduminiKulathunga <viduminikulathunga@gmail.com>
2022-10-08 18:32:51 +0200

    useMutation hook

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { server, useQuery } from "../../lib/api";
+import { useQuery, useMutation } from "../../lib/api";
 import {
   ListingsData,
   Listing,
@@ -34,14 +34,13 @@ interface Props {
 export const Listings = ({ title }: Props) => {
   const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
 
-  const deleteListings = async (id: string) => {
-    await server.fetch<DeleteListingData, DeleteListingVariable>({
-      query: DELETE_LISTING,
-      variables: {
-        id,
-      },
-    });
+  const [
+    deleteListings,
+    { loading: deleteListingLoading, error: deleteListingError },
+  ] = useMutation<DeleteListingData, DeleteListingVariable>(DELETE_LISTING);
 
+  const handleDeleteListing = async (id: string) => {
+    await deleteListings({ id });
     refetch();
   };
 
@@ -53,27 +52,37 @@ export const Listings = ({ title }: Props) => {
         return (
           <li key={listing.id}>
             {listing.title}
-            <button onClick={() => deleteListings(listing.id)}>Delete</button>
+            <button onClick={() => handleDeleteListing(listing.id)}>
+              Delete
+            </button>
           </li>
         );
       })}
     </ul>
   ) : null;
 
-  if(loading){
-    return <h2>Loading...</h2>
+  if (loading) {
+    return <h2>Loading...</h2>;
   }
 
-  if(error){
-    return(
-      <h2>Uh oh! something went wrong - Please tr again later :(</h2>
-    )
+  if (error) {
+    return <h2>Uh oh! something went wrong - Please tr again later :(</h2>;
   }
+
+  const deleteListingLoadingMessage = deleteListingLoading ? (
+    <h4>Delete in Progress</h4>
+  ) : null;
+
+  const deleteListingErrorMessage = deleteListingError ? (
+    <h4>Uh oh! Something went wrong with deleteing :(</h4>
+  ) : null;
 
   return (
     <div>
       <h2>{title}</h2>
       {listingsList}
+      {deleteListingLoadingMessage}
+      {deleteListingErrorMessage}
     </div>
   );
 };
